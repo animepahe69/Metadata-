769a5c9f56af784332335857e4e18f97af21f413
animepahe69 <tanjirokamado66999@gmail.com>
2024-09-18 11:00:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -77,11 +77,23 @@ Use These Keywords To Setup Custom File Name
 
     PROGRESS_BAR = """<b>\n
 ╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
-⪼ 🗃️ Sɪᴢᴇ: {1} | {2}
-⪼ ⏳️ Dᴏɴᴇ : {0}%
-⪼ 🚀 Sᴩᴇᴇᴅ: {3}/s
-⪼ ⏰️ Eᴛᴀ: {4}
-
+┣⪼ 🗃️ Sɪᴢᴇ: {1} | {2}
+┣⪼ ⏳️ Dᴏɴᴇ : {0}%
+┣⪼ 🚀 Sᴩᴇᴇᴅ: {3}/s
+┣⪼ ⏰️ Eᴛᴀ: {4}
+┣⪼ 🥺 joine Plz: @AshutoshGoswami24
+╰━━━━━━━━━━━━━━━➣ </b>"""
+    
+    
+    DONATE_TXT = """<b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
+    
+If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
+    
+<b>My UPI - PandaWep@ybl</b> """
+    
+    HELP_TXT = """<b>Hey</b> {}
+    
+Joine @AshutoshGoswami24 To Help """
 
 
 
